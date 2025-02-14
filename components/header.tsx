0f745873f2ba248d76961cce712ba2c64a0bc007
NeilRow56 <neilrowland56@gmail.com>
2025-02-14 08:12:48 +0000

    Layout updated for header, footer and theme-toggle

---
+++ components/header.tsx
@@ -0,0 +1,60 @@
+import Link from "next/link";
+
+import { ThemeToggle } from "@/components/theme-toggle";
+
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import { Menu } from "lucide-react";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
+
+export default function Header() {
+  return (
+    <header className="fixed inset-x-0 top-0 z-50 bg-background/20 py-6 backdrop-blur-sm">
+      <nav className="container mx-auto flex items-center justify-between">
+        <Sheet>
+          <SheetTrigger className="sm:hidden">
+            <Menu className="h-6 w-6" />
+          </SheetTrigger>
+          <SheetTitle></SheetTitle>
+          <SheetContent side="left">
+            <ul className="flex flex-col gap-3 text-sm">
+              <li className="font-sans text-2xl">
+                <SheetClose asChild>
+                  <Link href="/">Community</Link>
+                </SheetClose>
+              </li>
+            </ul>
+          </SheetContent>
+        </Sheet>
+        <div className="justify-between flex w-full">
+          <ul className="hidden items-center  gap-14 text-sm font-medium sm:flex">
+            <li className="font-serif text-lg font-bold">
+              <Link href="/">Community</Link>
+            </li>
+          </ul>
+
+          <div className="flex items-center justify-between gap-6">
+            <ThemeToggle />
+
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+
+            <SignedOut>
+              <SignInButton>
+                <Button>Sign in</Button>
+              </SignInButton>
+            </SignedOut>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
