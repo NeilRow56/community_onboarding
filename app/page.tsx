78f459d0b72ef9f76278b437ffe332d628e95b84
NeilRow56 <neilrowland56@gmail.com>
2025-02-14 13:57:22 +0000

    Added companies functionality - prior to members

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,40 @@
+import CompanyItem from "@/components/company-item";
+import { getCompanies } from "@/lib/companies";
+import Link from "next/link";
+
 export const metadata = {
   title: "HomePage",
 };
 
-const HomePage = () => {
+const HomePage = async () => {
+  const companies = await getCompanies();
   return (
-    <div className="mt-24">
-      <h1>Home page</h1>
-    </div>
+    <section className="pt-40 pb-40">
+      <div className="container">
+        <div>
+          <h1 className="font-serif text-3xl">New companies</h1>
+          <p className="mt-1 text-sm text-muted-foreground">
+            Recently joined companies
+          </p>
+
+          <ul className="4xl:grid-cols-3 mt-10 grid grid-cols-1 gap-6 md:grid-cols-2 xl:max-2xl:grid-cols-3">
+            {companies.map((company) => (
+              <li key={company.id}>
+                <CompanyItem company={company} />
+              </li>
+            ))}
+          </ul>
+
+          <Link
+            href="/companies"
+            className="mt-8 inline-flex items-center gap-2 transition-colors hover:text-fuchsia-500"
+          >
+            View all companies
+            <span aria-hidden="true">→</span>
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,6 +4,7 @@ import { PrismaNeon } from "@prisma/adapter-neon";
 import { Pool, neonConfig } from "@neondatabase/serverless";
 
 import ws from "ws";
+
 neonConfig.webSocketConstructor = ws;
 
 // To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
