483093feb238d6747e0d8e530fcecde33825a287
NeilRow56 <neilrowland56@gmail.com>
2025-02-15 16:27:19 +0000

    Updated api-webhooks-clerk

---
+++ app/api/webhooks/clerk/route.ts
@@ -0,0 +1,90 @@
+import { Webhook } from 'svix'
+import { headers } from 'next/headers'
+import { WebhookEvent } from '@clerk/nextjs/server'
+import prisma from '@/lib/prisma'
+import { createUser } from '@/lib/users'
+import { User } from '@prisma/client'
+import { revalidatePath } from 'next/cache'
+
+export async function POST(req: Request) {
+  const SIGNING_SECRET = process.env.SIGNING_SECRET
+
+  if (!SIGNING_SECRET) {
+    throw new Error(
+      'Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env.local'
+    )
+  }
+
+  // Create new Svix instance with secret
+  const wh = new Webhook(SIGNING_SECRET)
+
+  // Get headers
+  const headerPayload = await headers()
+  const svix_id = headerPayload.get('svix-id')
+  const svix_timestamp = headerPayload.get('svix-timestamp')
+  const svix_signature = headerPayload.get('svix-signature')
+
+  // If there are no headers, error out
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response('Error: Missing Svix headers', {
+      status: 400
+    })
+  }
+
+  // Get body
+  const payload = await req.json()
+  const body = JSON.stringify(payload)
+
+  let evt: WebhookEvent
+
+  // Verify payload with headers
+
+  try {
+    evt = wh.verify(body, {
+      'svix-id': svix_id,
+      'svix-timestamp': svix_timestamp,
+      'svix-signature': svix_signature
+    }) as WebhookEvent
+  } catch (err) {
+    console.error('Error verifying webhook:', err)
+    return new Response('Error occurred', {
+      status: 400
+    })
+  }
+
+  const eventType = evt.type
+
+  if (eventType === 'user.created') {
+    const { id, email_addresses, first_name, last_name, image_url } = evt.data
+
+    if (!id || !email_addresses || !email_addresses.length) {
+      return new Response('Error occurred -- missing data', {
+        status: 400
+      })
+    }
+
+    const email = email_addresses[0].email_address
+
+    const user = {
+      clerkUserId: id,
+      firstName: first_name,
+      lastName: last_name,
+      email: email,
+      ...(image_url ? { imageUrl: image_url } : {})
+    }
+
+    try {
+      const { user: userFromDB, error } = await createUser(user as User)
+      if (error) throw error
+      revalidatePath(`/`)
+    } catch (error) {
+      return new Response('Error occurred', {
+        status: 400
+      })
+    }
+
+    // Trigger a workflow
+  }
+
+  return new Response('Webhook received successfully', { status: 200 })
+}
